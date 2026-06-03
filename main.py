c0d5f3df71e20c9b368263cfd84ae0909dfb5faa
sanjiv576 <shresthasanjiv576@gmail.com>
2026-06-03 20:23:32 +0545

    update

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = FastAPI()
 
 
 @app.get('/')
 def home():
-    return JSONResponse(status_code=200, content={"message": "Everything is working fine."})
+    
+    return JSONResponse(status_code=200, content={"message": f"Everything is working fine: {os.environ.get("MONGO_URL")}"})
