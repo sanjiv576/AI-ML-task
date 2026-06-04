f6b07e04943a59d0d56ad338e577e60678fd235b
sanjiv576 <shresthasanjiv576@gmail.com>
2026-06-04 11:38:16 +0545

    Refactor main.py to include ingest router and improve environment variable checks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from app.models.database import ping_mongodb
+from app.api.routes_ingest import router as ingest_router
+
 import os
 from dotenv import load_dotenv
 
@@ -9,26 +11,25 @@ load_dotenv()
 
 app = FastAPI()
 
+app.include_router(ingest_router)
+
 
 @app.on_event("startup")
 async def startup_event():
     await ping_mongodb()
 
-    # required_env_vars = ["PINECONE_API_KEY", "OPENAI_API_KEY", "PINECONE_INDEX_NAME"]
-    # missing_vars = [var for var in required_env_vars if not os.getenv(var)]
+    required_env_vars = ["PINECONE_API_KEY",
+                         "OPENAI_API_KEY", "PINECONE_INDEX_NAME"]
+    missing_vars = [var for var in required_env_vars if not os.getenv(var)]
 
-    # if missing_vars:
-    #     print(f"CRITICAL WARNING: Missing environment configuration keys: {missing_vars}")
-    # else:
-    #     print("Initialization complete. All critical API keys are set up.")
+    if missing_vars:
+        print(
+            f"CRITICAL WARNING: Missing environment configuration keys: {missing_vars}")
+    else:
+        print("Initialization complete. All critical API keys are set up.")
 
 
 @app.get('/', tags=["Health"])
 def root():
 
-    return JSONResponse(status_code=200, content={"message": f"Everything is working fine: {os.environ.get("MONGO_URL")}"})
-
-
-@app.post('/upload')
-def file_upload():
-    pass
+    return JSONResponse(status_code=200, content={"message": f"Server is live at: {os.environ.get("MONGO_URL")}"})
